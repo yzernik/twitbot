c01500845ae92249c00763aefe74c14eabaad5c6
Sebastien Renault <sre@zenexity.com>
2015-01-30 16:27:15 +0100

    stream tweets

--- project.clj
+++ project.clj
@@ -3,7 +3,7 @@
   :url "http://github.com/gre/rmsbot"
   :license {:name "Affero General Public License"
             :url "https://gnu.org/licenses/agpl.html"}
-  :dependencies [[org.clojure/clojure "1.6.0"] [twitter-api "0.7.8"]]
+  :dependencies [[org.clojure/clojure "1.6.0"] [twitter-api "0.7.8"] [twitter-streaming-client "0.3.2"] [overtone "0.9.1"]]
   :main ^:skip-aot rmsbot.core
   :target-path "target/%s"
   :profiles {:uberjar {:aot :all}})
--- src/rmsbot/twitter.clj
+++ src/rmsbot/twitter.clj
@@ -5,12 +5,16 @@
     [twitter.callbacks]
     [twitter.callbacks.handlers]
     [twitter.api.streaming]
+    [overtone.at-at]
     [twitter.api.restful])
   (:require
+    [twitter-streaming-client.core :as client]
+    [twitter.oauth :as oauth]
     [clojure.data.json :as json]
     [http.async.client :as ac])
   (:import
-    (twitter.callbacks.protocols SyncSingleCallback)))
+    (twitter.callbacks.protocols SyncSingleCallback)
+    (twitter.callbacks.protocols AsyncStreamingCallback)))
 
 (require 'clojure.edn)
 
@@ -25,3 +29,19 @@
   [username]
   (users-show :oauth-creds my-creds :params {:screen-name username})
   )
+
+(defn create-stream [term]
+  (client/create-twitter-stream twitter.api.streaming/statuses-filter :oauth-creds my-creds :params {:track term}))
+
+(defn start-stream [stream]
+  (client/start-twitter-stream stream))
+
+(def my-pool (mk-pool))
+
+(defn stream [term callback]
+  (let [stream (create-stream term)
+        _ (start-stream stream)]
+
+    (every 1000 (fn[](let [q (client/retrieve-queues stream)
+                           tweets (:tweet q)]
+                       (doseq [t tweets] (callback t)))) my-pool)))
